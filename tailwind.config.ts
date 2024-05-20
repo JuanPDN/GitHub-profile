3314e67a1767da50d588e6e0ffde804af624cab3
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-05-20 17:41:26 -0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      "111729": "#111729",
+      "1D1B48": "#1D1B48",
+      "3662E3": "#3662E3",
+      "20293A": "#20293A",
+      "364153": "#364153",
+      "4A5567": "#4A5567",
+      "CDD5E0": "#CDD5E0",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
